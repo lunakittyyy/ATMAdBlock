2c217c3c2ee18e300d49cfa6bfc3e6b14d8ef5e2
Kaylie Craig <wowitskaylie@gmail.com>
2025-04-19 03:37:51 -0400

    Initial commit

---
+++ ATMAdBlock/PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace ATMAdBlock
+{
+	/// <summary>
+	/// This class is used to provide information about your mod to BepInEx.
+	/// </summary>
+	class PluginInfo
+	{
+		public const string GUID = "com.lunakittyyy.gorillatag.atmadblock";
+		public const string Name = "ATMAdBlock";
+		public const string Version = "1.0.0";
+	}
+}
